b6c5d8634904f53a8a0f6b7e5d1bcfde0ae238c4
Lukáš Vykydal <luksa.vyksa@seznam.cz>
2022-02-20 12:47:36 +0100

    Add DbConnection code for MongoDB

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,5 +1,39 @@
-﻿namespace SuggestionAppLibrary.DataAccess;
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace SuggestionAppLibrary.DataAccess;
 
 public class DbConnection
 {
+   private readonly IConfiguration _config;
+   private readonly IMongoDatabase _db;
+   private string _connectionId = "MongoDB";
+   public string DbName { get; private set; }
+
+   public MongoClient Client { get; private set; }
+
+   public IMongoCollection<Category> Categories { get; private set; }
+   public IMongoCollection<Status> Statuses { get; private set; }
+   public IMongoCollection<User> Users { get; private set; }
+   public IMongoCollection<Suggestion> Suggestions { get; private set; }
+
+
+   public string CategoryCollectionName { get; private set; } = "categories";
+   public string StatusCollectionName { get; private set; } = "statuses";
+   public string UserCollectionName { get; private set; } = "users";
+   public string SuggestionCollectionName { get; private set; } = "suggestions";
+
+
+   public DbConnection(IConfiguration config)
+   {
+      _config = config;
+      Client = new MongoClient(_config.GetConnectionString(_connectionId));
+      DbName = _config["DatabaseName"];
+      _db = Client.GetDatabase(DbName);
+
+      Categories = _db.GetCollection<Category>(CategoryCollectionName);
+      Statuses = _db.GetCollection<Status>(StatusCollectionName);
+      Users = _db.GetCollection<User>(UserCollectionName);
+      Suggestions = _db.GetCollection<Suggestion>(SuggestionCollectionName);
+   }
 }
