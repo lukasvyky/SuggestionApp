38f5128c461c27f191fb8eac1d112c6f8d2c9080
Lukáš Vykydal <luksa.vyksa@seznam.cz>
2022-05-21 13:14:57 +0200

    Add CreateSuggestion View model and page

---
+++ SuggestionAppUI/ViewModels/CreateSuggestionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.ViewModels;
+
+public class CreateSuggestionViewModel
+{
+   [Required]
+   [MaxLength(75)]
+   public string SuggestionContent { get; set; }
+
+   [Required]
+   [MinLength(1)]
+   [Display(Name = "Category")]
+   public string CategoryId { get; set; }
+
+   [MaxLength(500)]
+   public string Description { get; set; }
+}
